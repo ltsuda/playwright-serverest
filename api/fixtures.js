41058404fe2ee4860cb226e1940fadc9718ff1ff
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-10-27 00:51:25 -0300

    implement login, cadastrar usuario, produto e carrinho fixtures

--- api/fixtures.js
+++ api/fixtures.js
@@ -1,17 +1,77 @@
-const base = require('@playwright/test');
-const { expect } = require('@playwright/test');
+const base = require("@playwright/test")
+const { expect } = require("@playwright/test")
+const faker = require("faker")
 
 module.exports = base.test.extend({
-    login: async ({ baseURL, request }, use) => {
+    loginPath: "/login",
+    cartPath: "/carrinhos",
+    userPath: "/usuarios",
+    productsPath: "/produtos",
+
+    login: async ({ baseURL, request, loginPath }, use) => {
         await use(async (email, password) => {
-            const response = await request.post(`${baseURL}/login`, {
+            const response = await request.post(`${baseURL}${loginPath}`, {
                 data: {
-                    "email": email,
-                    "password": password
-                }
+                    email: email,
+                    password: password,
+                },
             })
-            expect(response.ok()).toBeTruthy()
+            expect(response.status()).toBe(200)
             return await response.json()
         })
-    }
+    },
+    cadastrarUsuario: async ({ baseURL, request, userPath }, use) => {
+        await use(async ({ administrador = false } = {}) => {
+            let data = {
+                nome: faker.name.findName(),
+                email: faker.internet.email(),
+                password: faker.random.alphaNumeric(16),
+                administrador: administrador ? String(administrador) : String(faker.datatype.boolean()),
+            }
+            const response = await request.post(`${baseURL}${userPath}`, {
+                data: data,
+            })
+            expect(response.status()).toBe(201)
+            responseData = await response.json()
+            data["_id"] = responseData["_id"]
+            return data
+        })
+    },
+    cadastrarProduto: async ({ baseURL, request, productsPath }, use) => {
+        await use(async (authorization) => {
+            let data = {
+                nome: faker.commerce.productName(),
+                preco: faker.commerce.price(undefined, undefined, 0),
+                descricao: faker.commerce.productDescription(),
+                quantidade: faker.datatype.number(1000),
+            }
+            const response = await request.post(`${baseURL}${productsPath}`, {
+                data: data,
+                headers: { Authorization: authorization },
+            })
+            expect(response.status()).toBe(201)
+            responseData = await response.json()
+            data["_id"] = responseData["_id"]
+            return data
+        })
+    },
+    cadastrarCarrinho: async ({ baseURL, request, cartPath }, use) => {
+        await use(async (authorization, produtos) => {
+            let data = { produtos: [] }
+            for (produto of produtos) {
+                data.produtos.push({
+                    idProduto: produto.id,
+                    quantidade: produto.quantidade,
+                })
+            }
+            const response = await request.post(`${baseURL}${cartPath}`, {
+                data: data,
+                headers: { Authorization: authorization },
+            })
+            expect(response.status()).toBe(201)
+            responseData = await response.json()
+            data["_id"] = responseData["_id"]
+            return data
+        })
+    },
 })
