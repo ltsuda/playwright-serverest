12f88849e88db4bf65d4ad40cdcb5503bca9e518
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-10-27 01:35:40 -0300

    format playwright config file and add npm script

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,9 @@
 const config = {
     testDir: "tests/api",
     retries: 1,
-    reporter: process.env.CI ? [["dot"], ["html", { outputFolder: 'test-results' }]] : [["line"], ["html", { outputFolder: 'test-results' }]],
+    reporter: process.env.CI
+        ? [["dot"], ["html", { outputFolder: "test-results" }]]
+        : [["line"], ["html", { outputFolder: "test-results" }]],
     workers: process.env.CI ? 2 : undefined,
 
     webServer: {
@@ -9,7 +11,7 @@ const config = {
         port: 3000,
         timeout: 60 * 1000,
         reuseExistingServer: !process.env.CI,
-        cwd: "."
+        cwd: ".",
     },
 
     use: {
