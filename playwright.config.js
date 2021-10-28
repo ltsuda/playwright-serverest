e1ba5db9cbefa4367003644655a0e4541c7a8e3f
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-10-28 01:17:18 -0300

    Revert "add ServeRest as submodule to run without monitoring"
    This reverts commit 44b2a3fed2d68e468683cb61b31972960292de8e.

--- playwright.config.js
+++ playwright.config.js
@@ -7,11 +7,11 @@ const config = {
     workers: process.env.CI ? 2 : undefined,
 
     webServer: {
-        command: "NODE_ENV=serverest-test npx serverest --nodoc",
+        command: "npx serverest --nodoc",
         port: 3000,
         timeout: 60 * 1000,
         reuseExistingServer: !process.env.CI,
-        cwd: "serverest",
+        cwd: ".",
     },
 
     use: {
